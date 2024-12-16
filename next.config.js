7e78ab3c19dc18bcf88c931d75447743acb6d6b3
leila.s.sousa@hotmail.com <leila.s.sousa@hotmail.com>
2024-12-16 13:23:55 -0500

    fixing deployment issue

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+    domains: ['localhost'],
   },
-  images: { unoptimized: true },
-};
+  experimental: {
+    serverActions: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
